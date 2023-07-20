fdc46c48f093327d4f5f800d14a0201311416dbb
季珂 <bolin9212@163.com>
2023-07-20 16:21:27 +0800

    (refactor) remove signup form
    just don't need it, use connector for register

--- ui/src/pages/Users/Register/index.tsx
+++ ui/src/pages/Users/Register/index.tsx
@@ -1,19 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Col } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
 import { usePageTags } from '@/hooks';
-import { Unactivate, WelcomeTitle, PluginRender } from '@/components';
+import { WelcomeTitle, PluginRender } from '@/components';
 import { guard } from '@/utils';
 
-import SignUpForm from './components/SignUpForm';
-
 const Index: React.FC = () => {
-  const [showForm, setShowForm] = useState(true);
   const { t } = useTranslation('translation', { keyPrefix: 'login' });
-  const onStep = () => {
-    setShowForm((bol) => !bol);
-  };
   usePageTags({
     title: t('sign_up', { keyPrefix: 'page_title' }),
   });
@@ -23,15 +17,9 @@ const Index: React.FC = () => {
   return (
     <Container style={{ paddingTop: '4rem', paddingBottom: '5rem' }}>
       <WelcomeTitle />
-
-      {showForm ? (
-        <Col className="mx-auto" md={6} lg={4} xl={3}>
-          <PluginRender type="Connector" className="mb-5" />
-          <SignUpForm callback={onStep} />
-        </Col>
-      ) : (
-        <Unactivate visible={!showForm} />
-      )}
+      <Col className="mx-auto" md={6} lg={4} xl={3}>
+        <PluginRender type="Connector" className="mb-5" />
+      </Col>
     </Container>
   );
 };
